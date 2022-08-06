cdf8ab9981048d336e16f23470f733b92d4257f4
Rosy-Elsa <110471741+Rosy-Elsa@users.noreply.github.com>
2022-08-06 15:49:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,21 +29,21 @@ streamlit.dataframe(fruits_to_show)
 
 
 # Display New Fruit menu by connecting to Fruityvice.com or Display Fruity Vice API response 
-streamlit.title('My Parents Healthy Diner')
-#Creating a variable fruity_choice to be used as input to FruityVice as an input API call
-fruit_choice = streamlit.text_input('What Fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
+streamlit.title('Fruityvice Fruit Advice')
+try:
+    fruit_choice = streamlit.text_input('What Fruit would you like information about?')  #Creating a variable fruity_choice to be used as input to FruityVice as an input API call
+    if not fruit_choice:
+          streamlit.error('Please select a valid Fruit choice')
+else:
+    streamlit.write('The user entered', fruit_choice)
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())    # This writes the json data to the screen
-
-#Take the json version of the response and convert to normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#Output the normalized data onto the screen
-streamlit.dataframe(fruityvice_normalized)
-
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  #Take the json version of the response and convert to normalize it
+    streamlit.dataframe(fruityvice_normalized)     #Output the normalized data onto the screen
+except URLError as e:
+    streamlit.error()
+    
 #Do not run any code beyond this point
 streamlit.stop()
 
